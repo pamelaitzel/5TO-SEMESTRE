0b509c5628786e1e5906b46191f81973947e4639
pame <135767172+pamelaitzel@users.noreply.github.com>
2024-10-03 21:49:32 -0600

    Update DataFrame.py

--- DataFrame.py
+++ DataFrame.py
@@ -1,10 +1,5 @@
-import numpy as np
 import pandas as pd
-
-# Crear un array NumPy de 3x3
-data = np.random.rand(3, 3)
-
-# Crear un DataFrame a partir del array NumPy
-df = pd.DataFrame(data, index=['Fila1', 'Fila2', 'Fila3'], columns=['Columna1', 'Columna2', 'Columna3'])
-
+data = {'Nombre': ['Ana', 'Juan', 'Luis'], 'Edad': [23, 34, 45]}
+df = pd.DataFrame(data)
 print(df)
+
